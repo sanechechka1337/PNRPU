9d37fcbf7a3d32bbf5de54792c6287983255d1aa
Александр Сенокосов <senokosov.megatron@gmail.com>
2026-08-11 10:57:46 +0500

    add labs

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+const int len_arr = 6;
+int arr[len_arr] = { 7, 3, 8, 9, 2, 5 };
+
+void QuickSort(begin, end)
+{
+
+}
+
+int main()
+{
+   
+}
---
+++ figure/figure/figure.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+void Raw_star(int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << '*';
+	}
+	cout << endl;
+}
+
+int main()
+{
+	system("chcp 1251>NULL");
+
+	int n;
+	cin >> n;
+
+	Raw_star(n);
+
+	for (int i = 0; i < n - 2; i++)
+	{
+		cout << '*';
+		for (int j = 0; j < n - 2; j++)
+		{
+			cout << ' ';
+		}
+		cout << '*' << endl;
+	}
+
+	Raw_star(n);
+}
